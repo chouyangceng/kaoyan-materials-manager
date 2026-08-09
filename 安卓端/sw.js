333094c1e7c67dce12ccaaeb6f81054a9c9348f5
chouyangceng <chouyangceng@users.noreply.github.com>
2026-08-09 16:21:36 +0800

    Initial commit: 考研电子资料管理系统 (双端 PWA 资料索引管理)

---
+++ 安卓端/sw.js
@@ -0,0 +1,1 @@
+self.addEventListener('install',e=>e.waitUntil(caches.open('kaoyan-v1').then(c=>c.addAll(['./','./index.html','./app.js','./manifest.json']))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
